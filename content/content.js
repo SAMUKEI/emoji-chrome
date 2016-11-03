59085f5bb225530e977ab099e8b1950487270982
YUTA-FUJITA <yuta.fujita.dvs@diverse-inc.com>
2016-11-03 20:57:21 +0900

    fix #1 サジェスト対応 & 微リファクタ

--- content/content.js
+++ content/content.js
@@ -1,43 +1,68 @@
-function replace() {
-  var siteName = $('meta[property="og:site_name"]').attr("content")
-  if( !siteName.match(/GitLab|GitHub/i)) { return; }
+function loadEmoji() {
+  return new Promise((resolve, reject) => {
+    // Github/Gitlabのみ対象
+    var siteName = $('meta[property="og:site_name"]').attr("content")
+    if( !siteName.match(/GitLab|GitHub/i)) { reject(); }
 
-  const KeyEmoji = "emoji";
-  chrome.runtime.sendMessage({method: "localStorage", key: KeyEmoji}, function(response) {
-    var emoji = JSON.parse(response.data);
-    if( emoji ) {
-        $('p').each(function() {
-            var text = $(this).html();
-            $(this).html(
-                text.replace(/:([a-z0-9A-Z_-]+):/g, function(match) {
-                  var isEmoji = emoji.hasOwnProperty(match);
-                  if( isEmoji ) {
-                    var _id = Math.random().toString(36).slice(-8);
-
-                    // クロスドメイン対応のため、XMLHttpRequest経由で画像取得
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', emoji[match], true);
-                    xhr.responseType = 'blob';
-                    xhr.onload = function(e) {
-                      var src = window.URL.createObjectURL(this.response);
-                      $("#" + _id)[0].src = src;
-                    };
-                    xhr.send();
-
-                    var img = "<img id=\"" + _id + "\" width=\"32\" height=\"32\" />"
-                    console.log("img = " + img);
-                    return img;
-                  }
-                  return match;
-                })
-            );
-        });
-     }
+    const KeyEmoji = "emoji";
+    chrome.runtime.sendMessage({method: "localStorage", key: KeyEmoji}, function(response) {
+      var emoji = JSON.parse(response.data);
+      if( emoji ) { 
+        resolve(emoji);
+      } else {
+        reject();
+      }
+    });
   });
 }
 
-// FIXME: 通信毎に呼び出しが行われるので、レンダリング速度低下する可能性がありそう
+function replace(emoji) {
+  $('p').each(function() {
+      var text = $(this).html();
+      $(this).html(
+          text.replace(/:([a-z0-9A-Z_-]+):/g, function(match) {
+            var isEmoji = emoji.hasOwnProperty(match);
+            if( isEmoji ) {
+              var _id = Math.random().toString(36).slice(-8);
+
+              // クロスドメイン対応のため、XMLHttpRequest経由で画像取得
+              var xhr = new XMLHttpRequest();
+              xhr.open('GET', emoji[match], true);
+              xhr.responseType = 'blob';
+              xhr.onload = function(e) {
+                var src = window.URL.createObjectURL(this.response);
+                $("#" + _id)[0].src = src;
+              };
+              xhr.send();
+
+              var img = "<img id=\"" + _id + "\" width=\"32\" height=\"32\" />"
+              console.log("img = " + img);
+              return img;
+            }
+            return match;
+          })
+      );
+  });
+}
+
+jQuery(function ($) {
+  loadEmoji().then(function(emoji) {
+    // サジェスト
+    var emojisList = $.map(emoji, function(value, name) {
+      return {'url':value, 'name':name.replace(/:/g, "")};
+    });
+    $('textarea').atwho({
+      at: ':',
+      displayTpl: "<li><img src='${url}' height='32' width='32'/> ${name} </li>",
+      insertTpl: ":${name}:",
+      data: emojisList
+    });
+  });
+});
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  // 通信完了時の書き換え
-  replace();
+  loadEmoji().then(function (emoji){
+    // 絵文字の書き換え
+    replace(emoji);
+  });
 });
